1c54fa8cbb7648cec4143fc3eb7988869c539b29
anxhi <anxhirr@gmail.com>
2023-12-02 13:30:58 +0100

    create new user shop

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,17 +1,7 @@
 import { component$ } from "@builder.io/qwik";
-import type { DocumentHead } from "@builder.io/qwik-city";
+import { type DocumentHead } from "@builder.io/qwik-city";
 
 import { useAuthSession, useAuthSignout } from "./plugin@auth";
-
-// export const onRequest: RequestHandler = (event) => {
-//   const session: Session | null = event.sharedMap.get("session");
-//   if (!session || new Date(session.expires) < new Date()) {
-//     throw event.redirect(
-//       302,
-//       `/api/auth/signin?callbackUrl=${event.url.pathname}`,
-//     );
-//   }
-// };
 
 export default component$(() => {
   const session = useAuthSession();
@@ -27,13 +17,9 @@ export default component$(() => {
             </h1>
             <button
               class="btn"
-              onClick$={() =>
-                signOut.submit({
-                  callbackUrl: "/",
-                })
-              }
+              onClick$={() => signOut.submit({ callbackUrl: "/" })}
             >
-              log out
+              Sign Out
             </button>
           </div>
         </div>
--- src/routes/plugin@auth.ts
+++ src/routes/plugin@auth.ts
@@ -1,9 +1,7 @@
 import { serverAuth$ } from "@builder.io/qwik-auth";
 import GitHub from "@auth/core/providers/github";
-// import Google from "@auth/core/providers/google";
 import type { Provider } from "@auth/core/providers";
 import { PrismaAdapter } from "@auth/prisma-adapter";
-// import { PrismaClient } from "@prisma/client";
 import { PrismaClient } from "@prisma/client/edge";
 
 export const prisma = new PrismaClient();
@@ -24,12 +22,34 @@ export const { onRequest, useAuthSession, useAuthSignin, useAuthSignout } =
       // }),
     ] as Provider[],
     callbacks: {
-      async redirect({ url, baseUrl }) {
-        console.log("REDIRECT", url, baseUrl);
+      async redirect({ baseUrl }) {
         return baseUrl;
       },
 
       session: async ({ session, user }) => {
+        console.log("user", user);
+        const shop = await prisma.shop.findFirst({
+          where: { users: { some: { id: user.id } } },
+        });
+        console.log("shop", shop);
+
+        if (!shop) {
+          console.log("creating shop");
+          await prisma.shop.create({
+            data: {
+              name: "My First Shop",
+              address: user.email,
+              baseCurrency: "ALL",
+              city: "",
+              description: "",
+              email: "",
+              phone: "",
+              ownerId: user.id,
+              // users: { connect: { id: user.id } },TODO: add this, also the role
+            },
+          });
+        }
+
         return {
           ...session,
           userId: user.id,
